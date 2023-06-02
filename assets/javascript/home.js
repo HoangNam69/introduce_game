1fc0058f5007bcb54bafd0d01bf8116a7fc6b27a
Le Hoang Nam <lehoangnam.31123@gmail.com>
2023-06-02 22:17:41 +0700

    style notifications, advertise and footer

--- assets/javascript/home.js
+++ assets/javascript/home.js
@@ -12,6 +12,24 @@ btnServer.addEventListener('click', (e) => {
     // sound click buttom
     const click_Sound = document.getElementById('clickSound');
     click_Sound.play();
-
 })
 
+
+// Xử lý hiện ứng scroll hiển thị nội dung
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 200;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("show");
+        } else {
+            reveals[i].classList.remove("show");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
